b1ae70c03a69502e7444f62d0302e4e6a509de95
Habibur Rahman <hrsshainik@gmail.com>
2022-06-23 02:26:43 +0600

    second commit

--- Context/StateContext.js
+++ Context/StateContext.js
@@ -1,15 +1,40 @@
 import React, {createContext, useContext, useState, useEffect} from "react";
 import {toast} from "react-hot-toast"
+import { Product } from "../components";
 
  const Context = createContext();
 
  export const StateContext =({children}) => {
     const [showCart, setShowCart] = useState(false);
-    const [cartItems, setCartItems] = useState();
+    const [cartItems, setCartItems] = useState([]);
     const [totalPrice, setTotalPrice] = useState();
     const [totalQuantities, setTotalQuantities] = useState();
     const [qty, setQty] = useState(0)
 
+
+    const onAdd = (product, quantity) => {
+        const checkProductInCart = cartItems.find((item) => item._id === product._id);
+
+        if(checkProductInCart) {
+            setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity);
+            setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity );
+
+            const updatedCartItems = cartItems.map((cartProduct) => {
+                if(cartProduct._id === product._id) return {
+                    ...cartProduct,
+                    quantity: cartProduct.quantity + quantity
+                }
+            })
+            setCartItems(updatedCartItems);
+        }
+        else {
+            product.quantity = quantity;
+            
+            setCartItems([...cartItems], {...product})
+        }
+        toast.success(`${qty} ${product.name} added to cart`)
+
+    }
     const incQty = () => {
         setQty((prevQty) => (prevQty+1))
     }
@@ -29,7 +54,8 @@ import {toast} from "react-hot-toast"
             totalQuantities,
             qty,
             decQty,
-            incQty
+            incQty,
+            onAdd
         }}
         >
             {children}
